5ee2a9c77b099558dd409e8d2e464a57e6ed2fa8
Joanne Cheng <chengjoanne2@gmail.com>
2012-10-16 11:09:48 -0600

    return blob if no filename is given

--- lib/little_graphs.rb
+++ lib/little_graphs.rb
@@ -9,13 +9,15 @@ class LittleGraphs
   end
 
   def draw(datapoints, filename = nil)
-    if filename.nil? then filename = "new.jpeg" end
     coordinates = define_coordinates(datapoints)
 
     f = QuickMagick::Image::solid(@width, @height, :white)
     f.draw_polyline(coordinates, :fill => :white, :stroke => :blue)
 
-    f.save(filename)
+    if filename.nil?
+      f.to_blob
+    else
+      f.save(filename)
   end
 
   def define_coordinates(datapoints)
